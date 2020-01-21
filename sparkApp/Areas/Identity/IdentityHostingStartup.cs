73eebf5805c9d7c6925f141a1ceadbedd1a695ce
madhav1709 <madhav1709@gmail.com>
2020-01-21 09:50:40 -0600

    Add project files.

---
+++ sparkApp/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using sparkApp.Data;
+
+[assembly: HostingStartup(typeof(sparkApp.Areas.Identity.IdentityHostingStartup))]
+namespace sparkApp.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
---
+++ sparkApp/Pages/ServiceTypes/Edit.cshtml.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using sparkApp.Data;
+using sparkApp.Models;
+
+namespace sparkApp
+{
+    public class EditModel : PageModel
+    {
+        private readonly sparkApp.Data.ApplicationDbContext _context;
+
+        public EditModel(sparkApp.Data.ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        [BindProperty]
+        public ServiceType ServiceType { get; set; }
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            ServiceType = await _context.ServiceType.FirstOrDefaultAsync(m => m.Id == id);
+
+            if (ServiceType == null)
+            {
+                return NotFound();
+            }
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            _context.Attach(ServiceType).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!ServiceTypeExists(ServiceType.Id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return RedirectToPage("./Index");
+        }
+
+        private bool ServiceTypeExists(int id)
+        {
+            return _context.ServiceType.Any(e => e.Id == id);
+        }
+    }
+}
